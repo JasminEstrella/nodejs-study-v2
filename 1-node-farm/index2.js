0be02bad007ac814ff23161d83d924356556d6d8
Jasmin <estrellajasmin@gmail.com>
2024-11-23 19:53:32 +0800

    add routing

--- 1-node-farm/index2.js
+++ 1-node-farm/index2.js
@@ -1,14 +1,33 @@
-const http = require('http'); 
-const url = require('url');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 ///////////////////
 // SERVER
 const port = 8000;
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+console.log(`${__dirname}/dev-data/data.json`);
+// const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/home") {
+    res.end("Hello from the home page!");
+  } else if (pathName === "/products") {
+    res.end("Hello from the products page!");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/plain",
+      "my-header": "hello-jas",
+    });
+    res.end("<h1>Not found!</h1>");
+  }
 });
 
 server.listen(port, () => {
-    console.log(`Listening to requests on port ${port}`);
+  console.log(`Listening to requests on port ${port}`);
 });
