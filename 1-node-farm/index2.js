52c12a57080caeda6a6dfbb35a3d2cdc4b6765e6
Jasmin <estrellajasmin@gmail.com>
2024-11-24 13:00:01 +0800

    add replaceTemplate function to modules

--- 1-node-farm/index2.js
+++ 1-node-farm/index2.js
@@ -1,25 +1,10 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
+const port = 8000;
+const replaceTemplate = require("./modules/replaceTemplate");
 ///////////////////
 // SERVER
-const port = 8000;
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-  if (!product.organic)
-    output = output.replace("{%NOT_ORGANIC%}", "not-organic");
-
-  return output;
-};
 
 const tempOverviewHtml = fs.readFileSync(
   `${__dirname}/templates/overview.html`,
@@ -35,7 +20,7 @@ const tempProductsHtml = fs.readFileSync(
 );
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-console.log(`${__dirname}/dev-data/data.json`);
+// console.log(`${__dirname}/dev-data/data.json`);
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
@@ -49,6 +34,7 @@ const server = http.createServer((req, res) => {
     const output = tempOverviewHtml.replace("{%CARDS%}", cardsHtml);
     res.end(output);
   } else if (pathname === "/product") {
+    console.log(req.method);
     res.writeHead(200, { "Content-type": "text/html " });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProductsHtml, product);
