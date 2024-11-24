84f6f9ce9932048e54e09b35f6edf56224bdd666
Jasmin <estrellajasmin@gmail.com>
2024-11-24 09:54:13 +0800

    parsing variables from urls

--- 1-node-farm/index2.js
+++ 1-node-farm/index2.js
@@ -6,14 +6,14 @@ const fs = require("fs");
 const port = 8000;
 
 const replaceTemplate = (temp, product) => {
-  let output = temp.replace("{%PRODUCTNAME%}", product.productName);
-  output = output.replace("{%ID%}", product.id);
-  output = output.replace("{%FROM%}", product.from);
-  output = output.replace("{%NUTRIENTS%}", product.nutrients);
-  output = output.replace("{%QUANTITY%}", product.quantity);
-  output = output.replace("{%PRICE%}", product.price);
-  output = output.replace("{%IMAGE%}", product.image);
-  output = output.replace("{%DESCRIPTION%}", product.description);
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
 
   if (!product.organic)
     output = output.replace("{%NOT_ORGANIC%}", "not-organic");
@@ -39,18 +39,21 @@ console.log(`${__dirname}/dev-data/data.json`);
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html " });
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCardsHtml, el))
       .join("");
     const output = tempOverviewHtml.replace("{%CARDS%}", cardsHtml);
     res.end(output);
-  } else if (pathName === "/products") {
-    res.end("Hello from the products page!");
-  } else if (pathName === "/api") {
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html " });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProductsHtml, product);
+    res.end(output);
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
   } else {
